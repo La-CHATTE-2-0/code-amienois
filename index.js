bc97f7608825ee34eef46acd26a2f61e89cdf9ab
Alan LECART <alan.lecart@outlook.com>
2022-10-02 16:10:46 +0200

    search header

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("Script loaded")
+
+//#region Init search
+const searchInput = document.getElementById("search-input");
+const searchSubmit = document.getElementById("search-submit");
+
+searchInput.addEventListener("input", (e) => {
+    if (e.target.value.length > 2) {
+        searchSubmit.classList.remove("disabled")
+    } else {
+        searchSubmit.classList.add("disabled")
+    }
+})
+
+searchSubmit.addEventListener("click", (e) => {
+    window.find(searchInput.value)
+})
+//#endregion Init search
