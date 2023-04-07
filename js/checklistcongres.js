f37cc00e29dafe9b183f824a72170714416eb483
Alan LECART <alan.lecart@outlook.com>
2023-04-07 10:22:45 +0200

    fix CH's code, null check and crash at load

--- js/checklistcongres.js
+++ js/checklistcongres.js
@@ -1,40 +1,46 @@
+import * as bootstrap from 'bootstrap'
+
 //#region ChecklistCongres
 const checkboxes = document.querySelectorAll('.checkcongres');
 const helpicons = document.querySelectorAll('.help-icon');
 
-if(localStorage.length > 0){
+if (localStorage.length > 0) {
     const keys = Object.keys(localStorage);
 
     keys.forEach(key => {
         const value = localStorage.getItem(key);
-        document.getElementById(key).checked = value;
+        const dom = document.getElementById(key)
+        if (dom != null)
+            dom.checked = value;
     });
 }
 
-checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('click', function() {
+checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('click', function () {
         localStorage.setItem(checkbox.id, checkbox.checked);
     });
 });
 
-helpicons.forEach(function(helpicon) {
-    helpicon.addEventListener('click', function() {
+helpicons.forEach(function (helpicon) {
+    helpicon.addEventListener('click', function () {
         let modal = new bootstrap.Modal(
-        document.getElementById("modal-info"),
-        {}
+            document.getElementById("modal-info"),
+            {}
         );
         document.getElementById('modal-content').innerHTML = helpicon.getAttribute('description');
         modal.show();
     });
 });
 
-document.getElementById('clear-checkboxes').addEventListener('click', function() {
+document.getElementById('clear-checkboxes').addEventListener('click', function () {
+    const excludedKeys = ['darkmode'];
     const keys = Object.keys(localStorage);
 
     keys.forEach(key => {
-        const value = localStorage.getItem(key);
-        document.getElementById(key).checked = false;
+        if (!excludedKeys.includes(key)) {
+            document.getElementById(key).checked = false;
+        }
     });
     localStorage.clear();
-  });
+});
 //#endregion ChecklistCongres
