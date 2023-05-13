ab3297c21c540efeee43bff3d70adc1070653ba0
Alan Lecart <alan.lecart@outlook.com>
2023-05-13 23:29:47 +0200

    petite optim

--- js/index.js
+++ js/index.js
@@ -1,33 +1,4 @@
-console.log("Bootstrap loading")
-//Keep bootstrap import, it is needed for header in mobile mode
-import * as bootstrap from 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
-console.log("Script loading")
-
-//#region Init search
-const searchInput = document.getElementById("search-input");
-const searchSubmit = document.getElementById("search-submit");
-const searchForm = document.getElementById("search-submit");
-
-searchInput && searchInput.addEventListener("input", (e) => {
-    if (e.target.value.length > 2) {
-        searchSubmit.classList.remove("disabled")
-    } else {
-        searchSubmit.classList.add("disabled")
-    }
-})
-
-searchSubmit && searchSubmit.addEventListener("click", (e) => {
-    e.preventDefault()
-    window.find(searchInput.value)
-})
-
-searchForm && searchForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    window.find(searchInput.value)
-})
-//#endregion Init search
 
 //#region Darkmode
 let darkmode = localStorage.getItem("darkmode");
