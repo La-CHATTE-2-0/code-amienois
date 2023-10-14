4f22c59d6d579f346369833176d2805c0a771c33
Alan Lecart <alan.lecart@outlook.com>
2023-10-14 22:23:12 +0200

    remove useless files and update dependcies

--- deploy-apache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Auto Copy script for the apache server
-
-echo "Start building"
-npm run clean-build 
-rm -rf /var/www/html/code-amienois
-cp -r ./dist /var/www/html/code-amienois
-
-echo "Done."
