b53d5ad23a5477321a0f0459d7f9a9844cab3362
Alan LECART <alan.lecart@outlook.com>
2023-11-21 14:33:58 +0100

    add easteregg sf

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,14 +1,26 @@
 console.log("Eastereggs loading");
-const image = document.getElementById("palmeSciences");
+const imageSciences = document.getElementById("palmeSciences");
+const imageSf = document.getElementById("ea-sf");
 
 let clicsSciences = 0;
+let clicsSf = 0;
 
-image.addEventListener("click", async () => {
+imageSciences.addEventListener("click", async () => {
     clicsSciences++;
 
     if (clicsSciences === 5) {
         const audio = new Audio("/sound/sciencesdecheance.mp3");
         await audio.play();
         clicsSciences = 0;
+    }
+});
+
+imageSf.addEventListener("click", async () => {
+    clicsSf++;
+
+    if (clicsSf === 5) {
+        const audio = new Audio("/sound/sf_pleurs_bebe_02.mp3");
+        await audio.play();
+        clicsSf = 0;
     }
 });
