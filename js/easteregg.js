78952e6747563433ec19f8dfd8039be78817ff0b
Alan LECART <alan.lecart@outlook.com>
2024-02-29 18:40:13 +0100

    some changes

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,9 +1,11 @@
 console.log("Eastereggs loading");
-const imageSciences = document.getElementById("palmeSciences");
+const imageSciences = document.getElementById("ea-palmeSciences");
 const imageSf = document.getElementById("ea-sf");
+const imageDroit = document.getElementById("ea-droits");
 
 let clicsSciences = 0;
 let clicsSf = 0;
+let clicsDroit = 0;
 
 imageSciences.addEventListener("click", async () => {
     clicsSciences++;
@@ -22,5 +24,15 @@ imageSf.addEventListener("click", async () => {
         const audio = new Audio("/sound/sf_pleurs_bebe_02.mp3");
         await audio.play();
         clicsSf = 0;
+    }
+});
+
+imageDroit.addEventListener("click", async () => {
+    clicsDroit++;
+
+    if (clicsDroit === 5) {
+        const audio = new Audio("/sound/interfi.mp3");
+        await audio.play();
+        clicsDroit = 0;
     }
 });
