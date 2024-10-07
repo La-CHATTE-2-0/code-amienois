8f44916a557d2621fc6b9a98df8beeb9d85fc3d2
Alan LECART <alan.lecart@outlook.com>
2024-10-07 19:45:18 +0200

    more étranger

---
+++ src/components/folklores/FolkloresEtrangers.Feluca.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-expansion-panels class="mb-5">
+        <v-expansion-panel elevation="0" variant="tonal">
+            <v-expansion-panel-title>
+                🇮🇹 La Goliarda Feluca
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+                <v-img
+                    class="my-5"
+                    src="/images/folklores/goliarda-cappello.jpg"
+                    alt="Goliarda"
+                ></v-img>
+
+                <p class="mt-5">
+                    La Goliarda est l'appellation usuelle donne aux ordres
+                    goliardiques, en italien ordini goliardici, sociétés
+                    festiveus et carnavalesques traditionnelle des étudiants
+                    italiens. L'appellation Goliarda fait référence aux goliards
+                    de l'ancien temps.
+                </p>
+
+                <p>
+                    Les membres de la Goliardia portent une coiffe appelée
+                    Feluca. Les premières coiffe goliardique a été imaginée par
+                    Guido Podrecca en 1888 en s'inspirant d'un modèle du xve
+                    siècle, elle s'appellait l'orsina. Elle se déclinait au
+                    choix en quatre couleurs possible :
+                </p>
+                <ul class="ml-8">
+                    <li>Bleu : Droit</li>
+                    <li>Rouge : Médecine</li>
+                    <li>Vert : Sciences</li>
+                    <li>Blanc : Lettres</li>
+                </ul>
+
+                <v-img
+                    class="my-5"
+                    src="/images/folklores/feluche.jpg"
+                    alt="Goliarda"
+                ></v-img>
+
+                <p class="mt-5">
+                    Il a existé aussi une autre coiffe goliardique, celle-ci
+                    originaire de Pise : le zucchetto.
+                </p>
+
+                <p class="mt-5">
+                    En 1891, un autre chapeau goliardique genre Robin des Bois
+                    est adoptée par la Goliardia de Sienne à l'occasion de la
+                    fête des nouveaux inscrits à l'université. Il porte alors le
+                    nom de Boccaccio. L'année d'après, lors des celebrazioni
+                    galileiane (célébrations galiléenes) à Padoue, les Padouans
+                    assurent sa généralisation dans la Goliardia. Et il élimine
+                    orsina etzucchetto. Ce nouveau chapeau aujourd'hui encore
+                    caractéristique de la Goliardia est finalement baptisé
+                    feluca, goliardo, pileo ou berretto universitario (béret
+                    universitaire). La couleur de la feluca indique quelles
+                    études suit celui qui la porte. Par exemple : rouge,
+                    médecine ; bleu, droit ; blanc, lettres ; vert, sciences ;
+                    jaune, économie ; noir, ingénieurs. Même si il existe, comme
+                    en France, des particularités dans certaines villes.
+                </p>
+
+                <p class="mt-5">
+                    La tenue de la Goliardia peut comporte également une placca
+                    (plaque), portant l'emblème de l'ordre auquel appartient
+                    celui ou celle qui la porte.
+                </p>
+            </v-expansion-panel-text>
+        </v-expansion-panel>
+    </v-expansion-panels>
+</template>
--- src/pages/FolkloresEtrangers.vue
+++ src/pages/FolkloresEtrangers.vue
@@ -2,6 +2,8 @@
 import FolkloresEtrangersCalottes from "../components/folklores/FolkloresEtrangers.Calottes.vue";
 import FolkloresEtrangersPenne from "../components/folklores/FolkloresEtrangers.Penne.vue";
 import FolkloresEtrangersPenneConsulaire from "../components/folklores/FolkloresEtrangers.PenneConsulaire.vue";
+import FolkloresEtrangersCzapka from "../components/folklores/FolkloresEtrangers.Czapka.vue";
+import FolkloresEtrangersFeluca from "../components/folklores/FolkloresEtrangers.Feluca.vue";
 </script>
 
 <template>
@@ -14,6 +16,8 @@ import FolkloresEtrangersPenneConsulaire from "../components/folklores/Folklores
         <FolkloresEtrangersCalottes></FolkloresEtrangersCalottes>
         <FolkloresEtrangersPenne></FolkloresEtrangersPenne>
         <FolkloresEtrangersPenneConsulaire></FolkloresEtrangersPenneConsulaire>
+        <FolkloresEtrangersCzapka></FolkloresEtrangersCzapka>
+        <FolkloresEtrangersFeluca></FolkloresEtrangersFeluca>
 
         <span class="text-caption">Crédits: Code Valenciennois</span>
     </v-container>
