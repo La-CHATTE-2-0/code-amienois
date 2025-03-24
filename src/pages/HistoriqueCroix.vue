aa30681585d197e223e75c88355a15c8e6488593
Alan Lecart <35492348+alanlecart@users.noreply.github.com>
2025-03-24 12:47:10 +0000

    upgrade node

--- src/pages/HistoriqueCroix.vue
+++ src/pages/HistoriqueCroix.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
-import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } from "../core/historique_croix";
+import {
+    sciences,
+    droits,
+    pharmaciens,
+    medecines,
+    sagefemmes,
+    intervelours,
+} from "../core/historique_croix";
 </script>
 
 <template>
@@ -7,7 +14,12 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
         <h2>Sciences</h2>
         <v-card class="pl-4">
             <v-timeline density="compact" side="end" align="start">
-                <v-timeline-item v-for="science in sciences" class="mb-4" dot-color="purple" size="small">
+                <v-timeline-item
+                    v-for="science in sciences"
+                    class="mb-4"
+                    dot-color="purple"
+                    size="small"
+                >
                     <div class="text-h6">{{ science.surname }}</div>
                     <p>
                         {{ science.year }}
@@ -19,7 +31,12 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
         <h2>Droits</h2>
         <v-card class="pl-4">
             <v-timeline density="compact" side="end" align="start">
-                <v-timeline-item v-for="droit in droits" class="mb-4" dot-color="red" size="small">
+                <v-timeline-item
+                    v-for="droit in droits"
+                    class="mb-4"
+                    dot-color="red"
+                    size="small"
+                >
                     <div class="text-h6">{{ droit.surname }}</div>
                     <p>
                         {{ droit.year }}
@@ -31,7 +48,12 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
         <h2>Sage-Femmes</h2>
         <v-card class="pl-4">
             <v-timeline density="compact" side="end" align="start">
-                <v-timeline-item v-for="sagefemme in sagefemmes" class="mb-4" dot-color="pink-darken-4" size="small">
+                <v-timeline-item
+                    v-for="sagefemme in sagefemmes"
+                    class="mb-4"
+                    dot-color="pink-darken-4"
+                    size="small"
+                >
                     <div class="text-h6">{{ sagefemme.surname }}</div>
                     <p v-if="sagefemme.year">
                         {{ sagefemme.year }}
@@ -43,8 +65,12 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
         <h2>Inter-Velours</h2>
         <v-card class="pl-4">
             <v-timeline density="compact" side="end" align="start">
-                <template v-for='iv in intervelours'>
-                    <v-timeline-item class="mb-4" :dot-color="iv.color" size="small">
+                <template v-for="iv in intervelours">
+                    <v-timeline-item
+                        class="mb-4"
+                        :dot-color="iv.color"
+                        size="small"
+                    >
                         <div class="text-h6">{{ iv.surname }}</div>
                         <p v-if="iv.year">
                             {{ iv.year }}
@@ -57,7 +83,12 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
         <h2>Pharmaciens</h2>
         <v-card class="pl-4">
             <v-timeline density="compact" side="end" align="start">
-                <v-timeline-item v-for="pharmacien in pharmaciens" class="mb-4" dot-color="green-darken-4" size="small">
+                <v-timeline-item
+                    v-for="pharmacien in pharmaciens"
+                    class="mb-4"
+                    dot-color="green-darken-4"
+                    size="small"
+                >
                     <div class="text-h6">{{ pharmacien.surname }}</div>
                     <p v-if="pharmacien.year">
                         {{ pharmacien.year }}
@@ -69,7 +100,12 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
         <h2>Medecines</h2>
         <v-card class="pl-4">
             <v-timeline density="compact" side="end" align="start">
-                <v-timeline-item v-for="medecine in medecines" class="mb-4" dot-color="red-darken-4" size="small">
+                <v-timeline-item
+                    v-for="medecine in medecines"
+                    class="mb-4"
+                    dot-color="red-darken-4"
+                    size="small"
+                >
                     <div class="text-h6">{{ medecine.surname }}</div>
                     <p v-if="medecine.year">
                         {{ medecine.year }}
@@ -78,8 +114,10 @@ import { sciences, droits, pharmaciens, medecines, sagefemmes, intervelours } fr
             </v-timeline>
         </v-card>
 
-        <p class="mt-8">Il a existé d'autres Grands-Maitres dans la ville (chez les jaunes et les IUTs). Je les
-            ajouterai si je
-            trouve assez d'information dessus.</p>
+        <p class="mt-8">
+            Il a existé d'autres Grands-Maitres dans la ville (chez les jaunes
+            et les IUTs). Je les ajouterai si je trouve assez d'information
+            dessus.
+        </p>
     </v-container>
 </template>
